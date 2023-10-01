a9c3c488e85b8f6e55f4686f44362a051fc07640
Lars Windolf <lars.windolf@gmx.de>
2023-10-01 18:47:53 +0200

    Introduce draggable split panes.

--- build.sh
+++ build.sh
@@ -3,6 +3,7 @@
 # Install deps
 test -d www/assets/js/lib || mkdir www/assets/js/lib
 cp node_modules/handlebars/dist/handlebars.min.js www/assets/js/lib
+cp node_modules/split.js/dist/split.es.js www/assets/js/lib
 
 platforms=$(cordova platform ls)
 for p in android electron browser; do
--- www/assets/js/layout.js
+++ www/assets/js/layout.js
@@ -9,8 +9,11 @@
 // both layouts at a threshold width as well as user agents
 // triggering it
 
+import Split from './lib/split.es.js';
+
 class Layout {
     static isSmall;
+    static split;
 
     static update() {
         const isMobile = /iPhone|iPad|iPod|Android/i.test(navigator.userAgent);
@@ -25,6 +28,9 @@ class Layout {
             theme.setAttribute('href', 'assets/css/mobile.css');
             Layout.isSmall = true
             Layout.view('feedlist');
+
+            if(Layout.split)
+                Layout.split.destroy();
         } else {
             theme.setAttribute('href', 'assets/css/desktop.css');
             Layout.isSmall = false;
@@ -32,6 +38,14 @@ class Layout {
             // show all views (desktop)
             [...document.querySelectorAll('.view')]
                 .forEach(e => (e.style.display = 'block'));
+
+            // setup split panes
+            Layout.split = Split(['#feedlist', '#itemlist', '#item'], {
+                sizes: [20, 30, 50],
+                minSize: [10, 10, 10],
+                gutterSize: 6,
+                expandToMin: true
+            });
         }
     }
 
